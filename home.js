d9b383f7e83b8c3a83839a779f996afeeefd3ec1
Resalat Al Samin <risalat731@gmail.com>
2025-08-25 22:31:18 +0600

    second push

--- home.js
+++ home.js
@@ -4,5 +4,12 @@ document.getElementById("add-money").addEventListener("click", function () {
   const bankAccount = document.getElementById("bank-account").value;
   const amount = parseInt(document.getElementById("amount").value);
   const pinNumber = document.getElementById("pin-number").value;
-  console.log(bankName, bankAccount, amount, pinNumber);
+  // console.log(bankName, bankAccount, amount, pinNumber);
+  const totalDollarAmount = parseInt(
+    document.getElementById("dollar-amount").innerText
+  );
+  // console.log(totalDollarAmount);
+  const totalNewAvailableBalance = amount + totalDollarAmount;
+
+  document.getElementById("dollar-amount").innerText = totalNewAvailableBalance;
 });
